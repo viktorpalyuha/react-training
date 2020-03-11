aa4bde93af62cd01c0408e9e2b2164939838e36d
Viktor Palyuha <viktorpalyuha@gmail.com>
2020-03-11 21:54:29 +0200

    Made delete component button and changing title while typing inside the input

--- src/App.js
+++ src/App.js
@@ -3,22 +3,66 @@ import './App.css';
 import Car from './Car/Car'
 
 class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'Ford', year: 2018 },
+      {name: 'Audi', year: 2016 },
+      {name: 'Mazda', year: 2010 }
+    ],
+    pageTitle: 'React components',
+    showCars: false
+  }
+
+  toggleCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    })
+  }
+
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name 
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({cars})
+  }
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+
+    this.setState({cars})
+  }
+
   render() {
     const divStyle = {
       textAlign: 'center'
     }
 
+    let cars = null
+
+    if(this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car 
+            key={index}
+            name={car.name}
+            year={car.year}
+            onChangeName={event => this.onChangeName(event.target.value, index)}
+            onDelete={this.deleteHandler.bind(this, index)}
+          />
+        )
+      }) 
+    }
+
     return (
     <div style={divStyle}>
-      <h1>HELLO WORLD</h1>
+      <h1>{this.state.pageTitle}</h1>
 
-      <Car name={'Ford'} year={2018}>
-        <p style={{color: 'blue'}}>COLOR</p>
-      </Car>
-      <Car name='Audi' year={2016}> 
-        <p style={{color: 'red'}}>COLOR</p>
-      </Car>
-      <Car name={'Mazda'} year={2010} />
+      <button onClick={this.toggleCarsHandler}>Toggle cars</button>
+
+      { cars }
     </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export default props => (
-    <div>
+    <div style={{
+        border: '1px solid #ccc',
+        marginBottom: '10px',
+        display: 'block',
+        padding: '10px'
+    }}>
         <h3>Car name: {props.name}</h3>   
         <p>YEAR: <strong>{props.year}</strong></p>
-        {props.children}
+        <input type="text" onChange={props.onChangeName} value={props.name}/>
+        <button onClick={props.onDelete}>Delete</button>
     </div>
 )
