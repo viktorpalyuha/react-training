e3ac6bf133a713c5e5ab5e77021279b00c2b5eeb
Viktor Palyuha <viktorpalyuha@gmail.com>
2020-03-12 19:07:37 +0200

    Add styling to car components

--- src/App.js
+++ src/App.js
@@ -62,7 +62,13 @@ class App extends Component {
 
       <button onClick={this.toggleCarsHandler}>Toggle cars</button>
 
-      { cars }
+      <div style={{
+        width: 400,
+        margin: 'auto',
+        paddingTop: '20px'
+      }}>
+        { cars }
+      </div>
     </div>
     );
   }
